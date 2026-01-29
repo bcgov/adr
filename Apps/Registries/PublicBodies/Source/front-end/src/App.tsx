50a51f89c9912fe1ea06e44f0524745422bbd33d
Tyler Krys <Tyler.Krys@gov.bc.ca>
2026-01-29 15:55:55 -0800

    Use PublicBodiesList component in App

--- Apps/Registries/PublicBodies/Source/front-end/src/App.tsx
+++ Apps/Registries/PublicBodies/Source/front-end/src/App.tsx
@@ -1,12 +1,16 @@
 import { Header, Footer } from "@bcgov/design-system-react-components";
 
+import PublicBodiesList from "./components/PublicBodiesList/PublicBodiesList";
+
 import "./App.css";
 
 function App() {
   return (
     <>
       <Header title="B.C. Public Bodies Register" />
-      <main />
+      <main>
+        <PublicBodiesList />
+      </main>
       <Footer />
     </>
   );
